efed906ab2f81ecd6e3af38c8f6bf774cc2dc61a
paul rublev <rublevps@gmail.com>
2023-01-22 19:19:56 +0500

    Add onTap method in track tile

--- diplom_spotify/lib/widgets/utility_widgets/track_list_tile.dart
+++ diplom_spotify/lib/widgets/utility_widgets/track_list_tile.dart
@@ -12,70 +12,71 @@ class TrackListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 65,
-      clipBehavior: Clip.antiAlias,
-      decoration: BoxDecoration(
-        color: !isFavorite
-            ? Theme.of(context).colorScheme.background
-            : Theme.of(context).dialogBackgroundColor,
-        borderRadius: BorderRadius.circular(10),
+    return GestureDetector(
+      onTap: () => showModalBottomSheet(
+        context: context,
+        backgroundColor: const Color(0x00000000),
+        useRootNavigator: true,
+        builder: (context) => BottomSheetPlayer(
+          track: track,
+        ),
       ),
-      margin: const EdgeInsets.only(
-        left: 25,
-        right: 25,
-        bottom: 15,
-      ),
-      child: Row(
-        children: [
-          SizedBox(
-            height: 65,
-            width: 65,
-            child: CustomCachedImage.album70x70(albumId: track.albumId),
-          ),
-          const SizedBox(width: 15),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  track.name,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  style: Theme.of(context).textTheme.subtitle2,
-                ),
-                const SizedBox(height: 5),
-                Text(
-                  track.albumName,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  style: Theme.of(context).textTheme.overline,
-                ),
-              ],
+      child: Container(
+        height: 65,
+        clipBehavior: Clip.antiAlias,
+        decoration: BoxDecoration(
+          color: !isFavorite
+              ? Theme.of(context).colorScheme.background
+              : Theme.of(context).dialogBackgroundColor,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        margin: const EdgeInsets.only(
+          left: 25,
+          right: 25,
+          bottom: 15,
+        ),
+        child: Row(
+          children: [
+            SizedBox(
+              height: 65,
+              width: 65,
+              child: CustomCachedImage.album70x70(albumId: track.albumId),
             ),
-          ),
-          Material(
-            color: !isFavorite
-                ? Theme.of(context).colorScheme.background
-                : Theme.of(context).dialogBackgroundColor,
-            child: IconButton(
-              splashRadius: 25,
-              padding: const EdgeInsets.all(18),
-              icon: const Icon(Icons.play_circle_outline_rounded),
-              onPressed: () {
-                showModalBottomSheet(
-                  context: context,
-                  backgroundColor: const Color(0x00000000),
-                  useRootNavigator: true,
-                  builder: (context) => BottomSheetPlayer(
-                    track: track,
+            const SizedBox(width: 15),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    track.name,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: Theme.of(context).textTheme.subtitle2,
                   ),
-                );
-              },
+                  const SizedBox(height: 5),
+                  Text(
+                    track.albumName,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: Theme.of(context).textTheme.overline,
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+            Material(
+              color: !isFavorite
+                  ? Theme.of(context).colorScheme.background
+                  : Theme.of(context).dialogBackgroundColor,
+              child: IconButton(
+                splashRadius: 25,
+                padding: const EdgeInsets.all(18),
+                icon: const Icon(Icons.play_circle_outline_rounded),
+                onPressed: () {},
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
